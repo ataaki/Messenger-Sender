1de4e47173b5982d93d95251c5b8d3790cf3f7af
ataaki <tilloy.pierre.olivier@protonmail.com>
2020-05-15 17:25:00 +0200

    added some functionnalities

--- messenger-sender.py
+++ messenger-sender.py
@@ -71,15 +71,69 @@ def findGroupIdFromName(gname):
 		print("No group found")
 		return
 
-def fetch_group_info():
-	users = client.fetchAllUsers()
-	userList = "{"
-	for user in users:
-		userList += "\"" + user.uid + "\":\"" + user.name + "\",\n "
-	userList += userList[:-1]+"}"
-	write_in_file("userList.json", userList, "w")
-	threads = client.fetchThreadList()
-	write_in_file("last20Convos.json", str(threads), "w")
+def fetch(type):
+	if type == 'users':
+		users = client.fetchAllUsers()
+		userList = "{"
+		for user in users:
+			userList += "\"" + user.uid + "\":\"" + user.name + "\",\n "
+		userList += userList[:-1]+"}"
+		try:
+			write_in_file("userList.json", userList, "w")
+			print("File userList.json has been generated")
+		except:
+			print("Error while writing file")
+	elif type == 'groups':
+		groupList = "{"
+		threads = client.fetchThreadList()
+		for group in threads:
+			if str(group)[:5] == 'Group':
+				groupList += "\"" + str(group.uid) + "\": \n{" + \
+				"\n\"Type\": \"" + str(group.type) + \
+				"\",\n\"Photo\": \"" + str(group.photo) + \
+				"\",\n\"Name\": \"" + str(group.name) + \
+				"\",\n\"Last_message_timestamp\": \"" + str(group.last_message_timestamp) + \
+				"\",\n\"Message_count\": \"" + str(group.message_count) + \
+				"\",\n\"Plan\": \"" + str(group.plan) + \
+				"\"\n},\n"
+		groupList = groupList[:-2] + "}"
+		write_in_file("groupList.json", str(groupList), "a")
+	elif type == 'archived':
+		archiveList = "{"
+		archives = client.fetchThreadList(thread_location=ThreadLocation.ARCHIVED)
+		for archive in archives:
+			#if str(groups)[:5] == 'Group':
+			archiveList += "\"" + str(archive.uid) + "\": \n{" + \
+			"\n\"Type\": \"" + str(archive.type) + \
+			"\",\n\"Photo\": \"" + str(archive.photo) + \
+			"\",\n\"Name\": \"" + str(archive.name) + \
+			"\",\n\"Last_message_timestamp\": \"" + str(archive.last_message_timestamp) + \
+			"\",\n\"Message_count\": \"" + str(archive.message_count) + \
+			"\",\n\"Plan\": \"" + str(archive.plan) + \
+			"\"\n},\n"
+		archiveList = archiveList[:-2] + "}"
+		write_in_file("archiveList.json", str(archiveList), "a")
+
+def fetchMessages(id):
+	messageList = "{"
+	messages = client.fetchThreadMessages(thread_id=id, limit=200)
+	for message in messages:
+		messageList += "\"" + str(message.text).replace("\"", "'") + "\": \n{" + \
+		"\n\"uid\": \"" + str(message.uid) + \
+		"\",\n\"mentions\": \"" + str(message.mentions) + \
+		"\",\n\"author\": \"" + str(message.author) + \
+		"\",\n\"timestamp\": \"" + str(message.timestamp) + \
+		"\",\n\"read_by\": \"" + str(message.read_by) + \
+		"\",\n\"sticker\": \"" + str(message.sticker) + \
+		"\",\n\"attachments\": \"" + str(message.attachments).replace("\"", "'") + \
+		"\",\n\"unsent\": \"" + str(message.unsent) + \
+		"\",\n\"reply_to_id\": \"" + str(message.reply_to_id) + \
+		"\",\n\"replied_to\": \"" + str(message.replied_to) + \
+		"\",\n\"forwarded\": \"" + str(message.forwarded) + \
+		"\"\n},\n"
+	messageList = messageList[:-2] + "}"
+	write_in_file(str(id)+"_message.json", str(messageList), "w")
+	#print(messages)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Send Facebook messenger from a command line. This program is base on fbchat module")
@@ -87,9 +141,10 @@ if __name__ == "__main__":
 	parser.add_argument('-p', '--password', help='specify password for the connection')
 	parser.add_argument('-c', '--cookies_file_path', help='specify a cookie file', nargs='?', const='./cookies')
 	parser.add_argument('-s', '--store_cookie',help='this will store cookies after the connection on your device (file is stored in program folder as \"./cookies\"', action='store_true')
-	parser.add_argument('-f', '--fetch_group',help='fetch group messages info', action='store_true')
+	parser.add_argument('-l', '--list_type',help='list info about groups/users/archived messages in json file', choices=['users', 'groups', 'archived'])
+	parser.add_argument('-g', '--get_data',help='fetch content from group/user messages. must use -uid', action="store_true")
 	parser.add_argument('-m', '--message',help='this will send the message specified')
-	parser.add_argument('-uid', '--user_id',help='specify to which user id you want to send the message', nargs='+')
+	parser.add_argument('-uid', '--user_id',help='specify a list of user id', nargs='+')
 	parser.add_argument('-uname', '--user_name',help='specify to which user name you want to send the message', nargs='+')
 	parser.add_argument('-gid', '--group_id',help='specify to which group id you want to send the message', nargs='+')
 	parser.add_argument('-gname', '--group_name',help='specify to which group name you want to send the message', nargs='+')
@@ -120,6 +175,9 @@ if __name__ == "__main__":
 					for name, id in groupDict.items():
 						print(name, id)
 						sendMessage(args.message, id, type=ThreadType.GROUP)
-			if args.fetch_group:
-				fetch_group_info()
+			if args.list_type:
+				fetch(args.list_type)
+			if args.get_data and args.user_id:
+				for uid in args.user_id:
+					fetchMessages(uid)
 
